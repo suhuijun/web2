3bcc6db8b877e11bf25b328310e0b4da064ba529
suhuijun <37681951+suhuijun@users.noreply.github.com>
2019-10-27 19:12:07 +0800

    Add files via upload

---
+++ build/precache-manifest.497d46492abd82dc787ad54d98456cd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f0d910d137237d8e2c5ce25cca5134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c35cec4d96811b17a78",
+    "url": "/static/css/main.456bba5b.chunk.css"
+  },
+  {
+    "revision": "767086180d8761e8c6e1",
+    "url": "/static/js/2.25e606dc.chunk.js"
+  },
+  {
+    "revision": "7c35cec4d96811b17a78",
+    "url": "/static/js/main.50f9c780.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
